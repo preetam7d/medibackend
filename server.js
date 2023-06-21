c04ef4536a3e16640d2d08e98f76594850de3bc7
preetam7d <yasasvipreetam3300@gmail.com>
2023-06-22 00:22:34 +0530

    updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = "mongodb+srv://Health_tracker:healthtracker@cluster0.ichwvvx.mongodb.net/nine?retryWrites=true&w=majority"
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
